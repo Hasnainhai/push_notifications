3e2ce35b1ea284d6be128fc4c96597291795c621
Hasnain Haider <codeWithHasnain>
2024-12-11 21:24:41 +0500

    working on in app notifications and kill-app notifications using FCM token

--- lib/notifications_services.dart
+++ lib/notifications_services.dart
@@ -1,9 +1,17 @@
+import 'dart:io';
+import 'dart:math';
+
 import 'package:app_settings/app_settings.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 class NotificationsServices {
   FirebaseMessaging messaging = FirebaseMessaging.instance;
-
+  final FlutterLocalNotificationsPlugin _flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+// notification permissions
   void requestNotificationsPermissions() async {
     NotificationSettings settings = await messaging.requestPermission(
       alert: true,
@@ -26,14 +34,88 @@ class NotificationsServices {
     }
   }
 
+// local notifications setup
+  void initLocalNotifications(
+      BuildContext context, RemoteMessage message) async {
+    var androidInitializeSettings =
+        const AndroidInitializationSettings('@mipmap/ic_launcher');
+    var iosInitializationSettings = const DarwinInitializationSettings();
+
+    var initializationSettings = InitializationSettings(
+      android: androidInitializeSettings,
+      iOS: iosInitializationSettings,
+    );
+    await _flutterLocalNotificationsPlugin.initialize(initializationSettings,
+        onDidReceiveNotificationResponse: (payload) {});
+  }
+
+// get FCM token or device token
   Future<String> getDeviceToken() async {
     String? token = await messaging.getToken();
     return token!;
   }
 
+// refresh FCM
   void refreshToken() {
     messaging.onTokenRefresh.listen((event) {
       print('Event: $event');
+    });
+  }
+
+// main func
+  void firebaseInit(BuildContext context) {
+    FirebaseMessaging.onMessage.listen((message) {
+      if (kDebugMode) {
+        print(message.notification!.title.toString());
+        print(message.notification!.body.toString());
+      }
+
+      if (Platform.isAndroid) {
+        initLocalNotifications(context, message);
+        showNotifications(message);
+      }
+    });
+  }
+
+// show notification fun
+  Future<void> showNotifications(RemoteMessage message) async {
+    const String channelId = 'high_alert_channel';
+    const String channelName = 'High Alert Notifications';
+
+    AndroidNotificationChannel channel = const AndroidNotificationChannel(
+      channelId,
+      channelName,
+      description: 'This channel is for high alert notifications',
+      importance: Importance.max,
+    );
+
+    AndroidNotificationDetails androidNotificationDetails =
+        AndroidNotificationDetails(
+      Random.secure().nextInt(10000000).toString(),
+      channel.name,
+      channelDescription: 'This is hasnain channel',
+      importance: Importance.high,
+      priority: Priority.high,
+      ticker: 'ticker',
+    );
+    DarwinNotificationDetails darwinNotificationDetails =
+        const DarwinNotificationDetails(
+      presentAlert: true,
+      presentBadge: true,
+      presentSound: true,
+    );
+    NotificationDetails notificationDetails = NotificationDetails(
+        android: androidNotificationDetails, iOS: darwinNotificationDetails);
+    // await _flutterLocalNotificationsPlugin.show(
+    //   0, // Unique notification id
+    //   message.notification!.title,
+    //   message.notification!.body,
+    //   notificationDetails,
+    // );
+
+    Future.delayed(Duration.zero, () {
+      _flutterLocalNotificationsPlugin.show(0, message.notification!.title,
+          message.notification!.body, notificationDetails);
     });
   }
 }
